4f982984aa4f7cf800f213d1339d3961c3a27b56
silentarrowz <silentarrowz@gmail.com>
2020-05-28 20:43:41 +0530

    added config for mysql db in heroku

--- db.js
+++ db.js
@@ -1,10 +1,17 @@
 const mysql = require('mysql');
+const config = require('./config');
+// const db = mysql.createConnection({
+//     host:'localhost',
+//     user:'faraz',
+//     password:'root',
+//     database:'agency-app'
+// });
 
 const db = mysql.createConnection({
-    host:'localhost',
-    user:'faraz',
-    password:'root',
-    database:'agency-app'
+    host: config.HOST, //'localhost',
+    user: config.USER,//'faraz',
+    password: config.PASSWORD, //'root',
+    database: config.DB //'agency-app'
 });
 
 db.connect((err)=>{
