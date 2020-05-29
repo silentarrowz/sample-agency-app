2bd41def1c6a1ce654c5a1ac7ea72483f8ddce73
silentarrowz <silentarrowz@gmail.com>
2020-05-29 13:35:56 +0530

    added config file to heroku

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    token:"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJjb2RpZnkiLCJpYXQiOjE1OTAyMzg1MzcsImV4cCI6MTYyMTc3NDUzNywiYXVkIjoid3d3LmNvZGlmeS5jb20iLCJzdWIiOiJmYXJhekBjb2RpZnkuY29tIn0.urPTGD8MDjlMgVO97dDwTi6VpVHXyb3DEYl3i-c_bDY",
+    secretKey:'qwertyuiopasdfghjklzxcvbnm123456',
+    HOST:'us-cdbr-east-05.cleardb.net',
+    USER:'b07de972c4821b',
+    PASSWORD: '0b3d37ec',
+    DB: 'heroku_07f3b1d9a5da8a5'
+}
